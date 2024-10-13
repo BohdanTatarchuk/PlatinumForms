707927a88d37b0dcf6a45f67f6ab8717b8ff5411
Roman <telishevskyir@gmail.com>
2024-10-13 20:19:12 +0200

    Log in page was updated.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import {MainComponent} from './main/main.component';
+import {StartComponent} from './start/start.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopHeaderComponent, MainComponent],
+  imports: [RouterOutlet, TopHeaderComponent, MainComponent, StartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
