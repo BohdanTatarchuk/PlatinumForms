d8975eaea2c3e9433847fa220a79542bbb5dbca6
Bohdan Tatarchuk <tatarchukbogdan80@gmail.com>
2024-10-16 00:21:52 +0200

    Main page was changed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,14 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import {MainComponent} from './main/main.component';
-import {StartComponent} from './start/start.component';
-import {RegistrationComponent} from './registration/registration.component';
-import {ProfileComponent} from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopHeaderComponent, MainComponent, StartComponent, RegistrationComponent, ProfileComponent],
+  imports: [RouterOutlet, TopHeaderComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/main/join-test/join-test.component.ts
+++ frontend/src/app/main/join-test/join-test.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {MyTestComponent} from '../my-tests/my-test/my-test.component';
 
 @Component({
   selector: 'app-join-test',
   standalone: true,
-  imports: [],
+  imports: [
+    MyTestComponent
+  ],
   templateUrl: './join-test.component.html',
   styleUrl: './join-test.component.css'
 })
+
 export class JoinTestComponent {
 
 }
