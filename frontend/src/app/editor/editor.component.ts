25408d6ed4a5f08303a389f9b094095995dca10f
Bohdan Tatarchuk <tatarchukbogdan80@gmail.com>
2024-10-16 21:48:36 +0200

    Editor page was updated.

--- frontend/src/app/editor/editor.component.ts
+++ frontend/src/app/editor/editor.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {TestHeadComponent} from '../test-head/test-head.component';
 import {TopHeaderComponent} from '../top-header/top-header.component';
+import {QuestionComponent} from "./question/question.component";
 
 @Component({
   selector: 'app-editor',
   standalone: true,
-  imports: [
-    TestHeadComponent,
-    TopHeaderComponent
-  ],
+    imports: [
+        TestHeadComponent,
+        TopHeaderComponent,
+        QuestionComponent
+    ],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.css'
 })
