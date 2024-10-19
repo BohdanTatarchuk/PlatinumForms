790ddcca49692db6531d65acb96baf292eab25d3
Roman <telishevskyir@gmail.com>
2024-10-19 16:35:23 +0200

    OnSubmit events were added in log in and registration components

--- frontend/src/app/start/log-in/log-in.component.ts
+++ frontend/src/app/start/log-in/log-in.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
 export class LogInComponent {
+  user = {
+    email: '',
+    password: ''
+  }
 
+  onSubmit() {
+    console.log('Email:', this.user.email);
+    console.log('Password:', this.user.password);
+  }
 }
