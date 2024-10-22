68749155caab5d803afc8214e32c05ee057fdd15
Bohdan Tatarchuk <tatarchukbogdan80@gmail.com>
2024-10-22 23:35:50 +0200

    generate question id function created

--- frontend/src/app/editor/question/question.component.ts
+++ frontend/src/app/editor/question/question.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {EditorComponent} from '../editor.component';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [],
+  imports: [ ],
   templateUrl: './question.component.html',
   styleUrl: './question.component.css'
 })
+
 export class QuestionComponent {
 
 }
+
+function generateQID(): string {
+  return  Date.now().toString(36) + Math.random().toString(36).substring(2);;
+}
+
+console.log('QuestionComponent generateQID:', generateQID());
