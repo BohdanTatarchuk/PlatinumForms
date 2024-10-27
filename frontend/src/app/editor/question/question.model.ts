cb513349af0dc5c69154ac22f92f51b8ee87c6b2
Roman <telishevskyir@gmail.com>
2024-10-27 22:24:41 +0100

    small update

--- frontend/src/app/editor/question/question.model.ts
+++ frontend/src/app/editor/question/question.model.ts
@@ -1,4 +1,3 @@
-import {Option} from '@angular/cli/src/command-builder/utilities/json-schema';
 
 export interface Question {
   name: string;
--- frontend/src/app/registration/registration-window/fake-data.ts
+++ frontend/src/app/registration/registration-window/fake-data.ts
@@ -4,26 +4,488 @@ export const FAKE_DATA = [
     username: "aaaaaAAAAA",
     password: "rrRR11!!",
     test: [
-      {
-        id: 'a1b2c3d4e5f6g7h8i9j0',
-        name: 'Mathematics 2B',
-        code: 'mth2b345',
-        description: 'basic math test',
-        questions: [
-          {
-            name: 'What is 2 + 2?',
-            id: 'q1a2b3c4',
-            obligatory: true,
-            type: 1
-          },
-          {
-            name: 'Calculate the value of 12 * 12.',
-            id: 'q5d6e7f8',
-            obligatory: false,
-            type: 1
-          }
-        ]
-      },
+      [
+        {
+          id: 'a1b2c3d4e5f6g7h8i9j0',
+          name: 'Mathematics 2B',
+          code: 'mth2b345',
+          description: 'basic math test',
+          questions: [
+            {
+              name: 'What is 2 + 2?',
+              id: 'q1a2b3c4',
+              obligatory: true,
+              type: 0,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            },
+            {
+              name: 'Calculate the value of 12 * 12.',
+              id: 'q5d6e7f8',
+              obligatory: false,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 'i9j8h7g6f5e4d3c2b1a0',
+          name: 'Science 3C',
+          code: 'sci3c456',
+          description: 'science basics',
+          questions: [
+            {
+              name: 'What is the chemical symbol for water?',
+              id: 'q2w3e4r5',
+              obligatory: true,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            },
+            {
+              name: 'Explain the process of photosynthesis.',
+              id: 'q8y7t6r5',
+              obligatory: false,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: '56789abcdef01234',
+          name: 'History 1A',
+          code: 'hst1a789',
+          description: 'introduction to history',
+          questions: [
+            {
+              name: 'Who was the first president of the United States?',
+              id: 'a12b34c56',
+              obligatory: true,
+              type: 1,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            },
+            {
+              name: 'Describe the causes of the Civil War.',
+              id: 'd78e90f12',
+              obligatory: true,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: '1122334455667788',
+          name: 'Geography 4B',
+          code: 'geo4b234',
+          description: 'world geography quiz',
+          questions: [
+            {
+              name: 'What is the capital of France?',
+              id: 'q98a76s54',
+              obligatory: false,
+              type: 1,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            },
+            {
+              name: 'Name the largest desert in the world.',
+              id: 'a45d67j89',
+              obligatory: true,
+              type: 1,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: '9988776655443322',
+          name: 'Physics 3A',
+          code: 'phy3a001',
+          description: 'introduction to physics',
+          questions: [
+            {
+              name: 'What is Newton’s second law of motion?',
+              id: 'r21f32d45',
+              obligatory: true,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            },
+            {
+              name: 'Define velocity and acceleration.',
+              id: 't45y67u89',
+              obligatory: false,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: '8765432109876543',
+          name: 'Chemistry 2C',
+          code: 'chem2c345',
+          description: 'basic chemistry test',
+          questions: [
+            {
+              name: 'What is the atomic number of hydrogen?',
+              id: 'x12c34z45',
+              obligatory: true,
+              type: 1,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            },
+            {
+              name: 'Explain the process of combustion.',
+              id: 'z76b89n90',
+              obligatory: false,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: '1234567890abcdef',
+          name: 'Computer Science 4A',
+          code: 'cs4a678',
+          description: 'introductory programming test',
+          questions: [
+            {
+              name: 'What is the Big O notation for binary search?',
+              id: 'f23s45d78',
+              obligatory: true,
+              type: 1,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            },
+            {
+              name: 'Explain the concept of recursion.',
+              id: 'g34h56j78',
+              obligatory: true,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 'abcdef1234567890',
+          name: 'Philosophy 1B',
+          code: 'phi1b345',
+          description: 'basic philosophy concepts',
+          questions: [
+            {
+              name: 'Who is considered the father of modern philosophy?',
+              id: 'b12n45m67',
+              obligatory: true,
+              type: 1,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            },
+            {
+              name: 'Describe Plato’s Allegory of the Cave.',
+              id: 't67j89u01',
+              obligatory: false,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: '56781234abcd5678',
+          name: 'Art 2D',
+          code: 'art2d901',
+          description: 'art history and appreciation',
+          questions: [
+            {
+              name: 'Who painted the Mona Lisa?',
+              id: 'h23k45l78',
+              obligatory: true,
+              type: 1,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            },
+            {
+              name: 'Explain the characteristics of the Baroque art style.',
+              id: 'm45b67n90',
+              obligatory: true,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: '91011121314151617',
+          name: 'Music 3C',
+          code: 'mus3c456',
+          description: 'introduction to music theory',
+          questions: [
+            {
+              name: 'What are the notes in a C major scale?',
+              id: 'j34m56n78',
+              obligatory: false,
+              type: 1,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            },
+            {
+              name: 'Describe the difference between melody and harmony.',
+              id: 'p12q34r56',
+              obligatory: true,
+              type: 2,
+              options: [
+                {
+                  name: '2',
+                  correct: false
+                },
+                {
+                  name: '4',
+                  correct: true
+                },
+                {
+                  name: '5',
+                  correct: false
+                }
+              ]
+            }
+          ]
+        }
+      ]
     ]
   },
   {
