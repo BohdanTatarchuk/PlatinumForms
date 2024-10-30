e28be681a98ad5cb0acc0d1e6b3056f0faa7d863
Roman <telishevskyir@gmail.com>
2024-10-30 10:31:31 +0100

    Google integration implemented

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -15,14 +15,14 @@ import {DUMMY_TESTS} from '../main/my-tests/my-test/dummy-data';
 export class ProfileComponent {
   constructor(public globalSercive: GlobalService) {}
 
-  users = DUMMY_TESTS;
   imageUrl: string = "";
   username: string | undefined ;
   email: string = "";
 
   ngOnInit(){
     this.email = this.globalSercive.email;
-    this.username = this.users.find(user => user.email ==this.email)?.username
+    this.username = this.globalSercive.username;
+    this.imageUrl = this.globalSercive.photo;
   }
 
   onFileSelected(event: Event): void {
@@ -31,7 +31,9 @@ export class ProfileComponent {
       const file = input.files[0];
       const reader = new FileReader();
       reader.onload = (e: any) => {
+        this.imageUrl = this.globalSercive.photo;
         this.imageUrl = e.target.result;
+        this.globalSercive.photo = this.imageUrl;
       };
       reader.readAsDataURL(file);
     }
--- frontend/src/app/start/log-in/log-in.component.ts
+++ frontend/src/app/start/log-in/log-in.component.ts
@@ -2,7 +2,8 @@ import {Component, inject} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {Router, RouterOutlet} from '@angular/router';
 import {GlobalService} from '../../services/global.service';
-import { DUMMY_TESTS } from '../../main/my-tests/my-test/dummy-data';
+import {DUMMY_TESTS} from '../../main/my-tests/my-test/dummy-data';
+import {gapi, loadGapiInsideDOM} from 'gapi-script';
 
 @Component({
   selector: 'app-log-in',
@@ -26,6 +27,32 @@ export class LogInComponent {
     password: ''
   }
 
+  ngOnInit() {
+    loadGapiInsideDOM().then(() => {
+      gapi.load('auth2', () => {
+        gapi.auth2.init({
+          client_id: '615371380297-rt8hqcr3o2i972rgobehnmudi2kl2d8i.apps.googleusercontent.com',
+          scope: 'profile email',
+          ux_mode: 'popup'
+        });
+      });
+    });
+  }
+
+  async handleGoogleSignIn() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    const googleUser = await auth2.signIn();
+    const profile = googleUser.getBasicProfile();
+    this.globalService.is_logged = true;
+    this.globalService.email = profile.getEmail();
+    this.globalService.username = profile.getName();
+    this.globalService.photo = profile.getImageUrl();
+    console.log(profile.getEmail());
+    console.log(profile.getName());
+    console.log(profile.getImageUrl());
+    this.router.navigate(['/main']);
+  }
+
   users = DUMMY_TESTS;
 
   data_check() {
@@ -34,10 +61,11 @@ export class LogInComponent {
       this.globalService.is_logged = true;
       this.globalService.email = this.data.email;
       this.router.navigate(['/main']);
-    }else console.log("AAAAAAAAAA")
+    } else console.log("AAAAAAAAAA")
   }
 
   navigateToRegistration() {
     this.router.navigate(['/registration']);
   }
+
 }
