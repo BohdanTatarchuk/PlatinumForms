c0f6be9eb962a8a7e6ae9424d925b0066c8b1b3d
Bohdan Tatarchuk <tatarchukbogdan80@gmail.com>
2024-12-02 15:09:12 +0100

    Fixed position removed, instead flex layout used. Profile link changed

--- frontend/src/app/main/profile/profile.component.ts
+++ frontend/src/app/main/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {Router} from '@angular/router';
 import {GlobalService} from '../../services/global.service';
+import {User} from '../../registration/registration-window/user.model';
 
 @Component({
   selector: 'app-profile',
@@ -10,16 +11,18 @@ import {GlobalService} from '../../services/global.service';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-
-
   constructor(public globalSercive: GlobalService) {}
 
+  username: string = "";
+  email: string = "";
   imageUrl: string = "";
 
   router = inject(Router)
 
   ngOnInit(){
     this.imageUrl = this.globalSercive.photo;
+    this.username = this.globalSercive.username;
+    this.email = this.globalSercive.email;
   }
 
   navigate_to_profile(){
