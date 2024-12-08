4f602aaa46274ad5ace6ed7fb336e43d2cc44f2d
Bohdan Tatarchuk <tatarchukbogdan80@gmail.com>
2024-12-08 12:42:31 +0100

    Profile page changed. User can change username

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -3,18 +3,20 @@ import {TopHeaderComponent} from '../top-header/top-header.component';
 import {GlobalService} from '../services/global.service';
 import {DUMMY_TESTS} from '../main/my-tests/my-test/dummy-data';
 import {Router} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [
-    TopHeaderComponent
+    TopHeaderComponent,
+    FormsModule
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-  constructor(public globalSercive: GlobalService) {}
+  constructor(public globalService: GlobalService) {}
 
   router = inject(Router)
 
@@ -22,10 +24,13 @@ export class ProfileComponent {
   username: string | undefined ;
   email: string = "";
 
+  newUsername: string = "";
+  emptyError: string = "";
+
   ngOnInit(){
-    this.email = this.globalSercive.email;
-    this.username = this.globalSercive.username;
-    this.imageUrl = this.globalSercive.photo;
+    this.email = this.globalService.email;
+    this.username = this.globalService.username;
+    this.imageUrl = this.globalService.photo;
   }
 
   onFileSelected(event: Event): void {
@@ -34,19 +39,29 @@ export class ProfileComponent {
       const file = input.files[0];
       const reader = new FileReader();
       reader.onload = (e: any) => {
-        this.imageUrl = this.globalSercive.photo;
+        this.imageUrl = this.globalService.photo;
         this.imageUrl = e.target.result;
-        this.globalSercive.photo = this.imageUrl;
+        this.globalService.photo = this.imageUrl;
       };
       reader.readAsDataURL(file);
     }
   }
 
   logout() {
-    this.globalSercive.email = "";
-    this.globalSercive.photo = "";
-    this.globalSercive.username = "";
-    this.globalSercive.is_logged = false;
+    this.globalService.email = "";
+    this.globalService.photo = "";
+    this.globalService.username = "";
+    this.globalService.is_logged = false;
     this.router.navigate(['/']);
+  }
+
+  changeUsername() {
+    if (this.newUsername) {
+      this.globalService.username = this.newUsername;
+      this.username = this.newUsername;
+      this.emptyError = "";
+    } else {
+      this.emptyError = "Username can not be empty";
+    }
   }
 }
